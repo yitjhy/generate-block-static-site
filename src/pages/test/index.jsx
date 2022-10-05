0fe168668a820ac2d7915698fa4e8a091e8092b7
yitjhy@126.com <yitjhy@126.com>
2022-10-05 22:03:24 +0800

    测试

--- src/pages/test/index.jsx
+++ src/pages/test/index.jsx
@@ -5,7 +5,7 @@ import codes from './../../codes/codes.json';
 
 import Testindex from './demo/testindex';
 
-let introductionStr = `# 测试23re`;
+let introductionStr = `# 测试gitlab`;
 let html = marked(introductionStr, {
     renderer: new marked.Renderer(),
     gfm: true,

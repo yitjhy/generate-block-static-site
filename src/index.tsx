dd27ee7d226be494591ea948763b4d70add8fc80
yitjhy@126.com <yitjhy@126.com>
2022-12-31 21:40:49 +0800

    修改生成方式

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-import './default.css'
 import 'antd/dist/antd.css'
 import { HashRouter } from 'react-router-dom'
 

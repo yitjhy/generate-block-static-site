c42fe53b3a106cb8f2536cec56e63d5e578573e8
yitjhy <yitjhy@126.com>
2023-02-20 08:59:38 +0000

    fix: 修改tabs片段

--- docs/tabs/demo/components/tabs.tsx
+++ docs/tabs/demo/components/tabs.tsx
@@ -1,5 +1,105 @@
 import styled from 'styled-components'
-import React, { useState, useEffect, FC } from 'react'
+import React, { useState, useEffect, FC, memo } from 'react'
+
+export interface TabsItemProps {
+  key: React.Key
+  label: React.ReactNode | string
+  children: React.ReactNode | string
+}
+
+export type tabsHeaderProps = Omit<TabsItemProps, 'children'>
+
+interface TabsProps {
+  items: TabsItemProps[]
+  defaultTab?: string | number
+  onChange?: (val: tabsHeaderProps) => void
+}
+
+const Tabs: FC<TabsProps> = ({ items, onChange, defaultTab }) => {
+  const [hadRenderTabPane, setHadRenderTabPane] = useState<(string | number)[]>(
+    defaultTab ? [defaultTab] : [items[0].key]
+  )
+  const [checkedId, setCheckedId] = useState<string | number>(defaultTab || items[0].key)
+  const [offsetLeft, setOffsetLeft] = useState(0)
+  const [offsetWidth, setOffsetWidth] = useState(0)
+
+  const tabsHeaderData: tabsHeaderProps[] = items.map((item) => {
+    return { key: item.key, label: item.label }
+  })
+
+  const getDefaultActiveKey = () => {
+    const activeKey = items[0]?.key
+    setCheckedId(activeKey)
+    return activeKey
+  }
+
+  const handleClick = (data: tabsHeaderProps, e: React.MouseEvent<HTMLDivElement>) => {
+    setHadRenderTabPane([...hadRenderTabPane, data.key])
+    setOffsetLeft((e.target as HTMLDivElement).offsetLeft)
+    setOffsetWidth((e.target as HTMLDivElement).offsetWidth)
+    setCheckedId(data.key)
+    onChange && onChange(data)
+  }
+
+  useEffect(() => {
+    !defaultTab && getDefaultActiveKey()
+  }, [items])
+  useEffect(() => {
+    if (defaultTab) {
+      setCheckedId(defaultTab)
+      setHadRenderTabPane([...hadRenderTabPane, defaultTab])
+    }
+  }, [defaultTab])
+  return (
+    <>
+      <TabsContentWrapper>
+        <TabsWrapper>
+          {tabsHeaderData.map((item) => {
+            return (
+              <TabsItem
+                isChecked={item.key === checkedId}
+                key={item.key}
+                ref={(node) => {
+                  if (checkedId === item.key) {
+                    setTimeout(() => {
+                      const offsetWidth = (node as HTMLDivElement)?.offsetWidth
+                      const offsetLeft = (node as HTMLDivElement)?.offsetLeft
+                      if (offsetWidth) {
+                        setOffsetWidth(offsetWidth)
+                        setOffsetLeft(offsetLeft)
+                      }
+                    }, 100)
+                  }
+                }}
+                onClick={(e) => {
+                  handleClick(item, e)
+                }}
+              >
+                {item.label}
+              </TabsItem>
+            )
+          })}
+        </TabsWrapper>
+        <Line left={offsetLeft} width={offsetWidth} />
+        {hadRenderTabPane.length > 0 &&
+          items.map((item) => {
+            return (
+              <>
+                {hadRenderTabPane.includes(item.key) && (
+                  <div key={item.key} style={{ display: item.key === checkedId ? 'block' : 'none' }}>
+                    {item.children}
+                  </div>
+                )}
+              </>
+              // <div key={item.key} style={{ display: item.key === checkedId ? 'block' : 'none' }}>
+              //   {item.children}
+              // </div>
+            )
+          })}
+      </TabsContentWrapper>
+    </>
+  )
+}
 
 const TabsContentWrapper = styled.div`
   width: 100%;
@@ -13,11 +113,10 @@ const TabsWrapper = styled.div`
 const TabsItem = styled.div<{ isChecked: boolean }>`
   font-style: normal;
   font-weight: 400;
-  font-size: 14px;
+  font-size: 16px;
   line-height: 20px;
   text-transform: capitalize;
   color: ${({ isChecked }) => (isChecked ? '#1677ff' : '#252525')};
-  text-shadow: 0 0 0.25px currentcolor;
   margin-right: 52px;
   cursor: pointer;
   transition: all linear 0.15s;
@@ -36,101 +135,16 @@ const Line = styled.div<{ left: number; width: number }>`
   background: #e2e2e2;
   position: relative;
   margin-top: 10px;
-  margin-bottom: 16px;
   &::after {
     content: '';
     position: absolute;
     width: ${({ width }) => width + 'px'};
     height: 2px;
-    background: #1677ff;
+    background: #73d37b;
     left: ${({ left }) => left + 'px'};
     top: 0;
     transition: all linear 0.15s;
   }
 `
 
-export interface TabsItemProps {
-  key: string
-  label: React.ReactNode | string
-  children: JSX.Element | string
-}
-
-export type tabsHeaderProps = Omit<TabsItemProps, 'children'>
-
-interface TabsProps {
-  items: TabsItemProps[]
-  defaultTab?: string
-  onChange?: (val: tabsHeaderProps) => void
-}
-
-const Tabs: FC<TabsProps> = ({ items, onChange, defaultTab }) => {
-  const [checkedId, setCheckedId] = useState<string>()
-  const [offsetLeft, setOffsetLeft] = useState(0)
-  const [offsetWidth, setOffsetWidth] = useState(0)
-
-  const tabsHeaderData: tabsHeaderProps[] = items.map((item) => {
-    return {
-      key: item.key,
-      label: item.label,
-    }
-  })
-
-  const getDefaultActiveKey = () => {
-    const activeKey = items[0]?.key
-    setCheckedId(activeKey)
-    return activeKey
-  }
-
-  const handleClick = (data: tabsHeaderProps, e: React.MouseEvent<HTMLDivElement>) => {
-    setOffsetLeft((e.target as HTMLDivElement).offsetLeft)
-    setOffsetWidth((e.target as HTMLDivElement).offsetWidth)
-    setCheckedId(data.key)
-    if (onChange) onChange(data)
-  }
-
-  useEffect(() => {
-    if (defaultTab) setCheckedId(defaultTab)
-    if (!defaultTab) getDefaultActiveKey()
-  }, [defaultTab])
-  return (
-    <>
-      <TabsContentWrapper>
-        <TabsWrapper>
-          {tabsHeaderData.map((item) => {
-            return (
-              <TabsItem
-                isChecked={item.key === checkedId}
-                key={item.key}
-                ref={(node) => {
-                  if (checkedId === item.key) {
-                    const offsetWidth = (node as HTMLDivElement)?.offsetWidth
-                    const offsetLeft = (node as HTMLDivElement)?.offsetLeft
-                    if (offsetWidth) {
-                      setOffsetWidth(offsetWidth)
-                      setOffsetLeft(offsetLeft)
-                    }
-                  }
-                }}
-                onClick={(e) => {
-                  handleClick(item, e)
-                }}
-              >
-                {item.label}
-              </TabsItem>
-            )
-          })}
-        </TabsWrapper>
-        <Line left={offsetLeft} width={offsetWidth} />
-        {items.map((item) => {
-          return (
-            <div key={item.key} style={{ display: item.key === checkedId ? 'block' : 'none' }}>
-              {item.children}
-            </div>
-          )
-        })}
-      </TabsContentWrapper>
-    </>
-  )
-}
-
-export default Tabs
+export default memo(Tabs)
--- docs/tabs/demo/demo.tsx
+++ docs/tabs/demo/demo.tsx
@@ -5,52 +5,52 @@ const Demo = () => {
       key: 'categories',
       label: 'Top Categories',
       children: (
-        <>
+        <div style={{ padding: '10px 5px' }}>
           <div>Top Categories Top Categories Top Categories</div>
           <div>Top Categories Top Categories Top Categories</div>
           <div>Top Categories Top Categories Top Categories</div>
           <div>Top Categories Top Categories Top Categories</div>
           <div>Top Categories Top Categories Top Categories</div>
-        </>
+        </div>
       ),
     },
     {
       key: 'sales',
       label: 'Top Sales',
       children: (
-        <>
+        <div style={{ padding: '10px 5px' }}>
           <div>Top Sales Top Sales Top Sales Top Sales Top Sales</div>
           <div>Top Sales Top Sales Top Sales Top Sales Top Sales</div>
           <div>Top Sales Top Sales Top Sales Top Sales Top Sales</div>
           <div>Top Sales Top Sales Top Sales Top Sales Top Sales</div>
           <div>Top Sales Top Sales Top Sales Top Sales Top Sales</div>
-        </>
+        </div>
       ),
     },
     {
       key: 'owners',
       label: 'Top Owners',
       children: (
-        <>
+        <div style={{ padding: '10px 5px' }}>
           <div>Top Owners Top Owners Top Owners Top Owners Top Owners</div>
           <div>Top Owners Top Owners Top Owners Top Owners Top Owners</div>
           <div>Top Owners Top Owners Top Owners Top Owners Top Owners</div>
           <div>Top Owners Top Owners Top Owners Top Owners Top Owners</div>
           <div>Top Owners Top Owners Top Owners Top Owners Top Owners</div>
-        </>
+        </div>
       ),
     },
     {
       key: 'gainers',
       label: 'Top Gainers',
       children: (
-        <>
+        <div style={{ padding: '10px 5px' }}>
           <div>Top Gainers Top Gainers Top Gainers Top Gainers Top Gainers</div>
           <div>Top Gainers Top Gainers Top Gainers Top Gainers Top Gainers</div>
           <div>Top Gainers Top Gainers Top Gainers Top Gainers Top Gainers</div>
           <div>Top Gainers Top Gainers Top Gainers Top Gainers Top Gainers</div>
           <div>Top Gainers Top Gainers Top Gainers Top Gainers Top Gainers</div>
-        </>
+        </div>
       ),
     },
   ]
